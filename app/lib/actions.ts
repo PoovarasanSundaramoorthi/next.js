179efa2636f6fccd3d4df1a88bb29d88d6cebc19
Poovarasan S <112718552+PoovarasanSundaramoorthi@users.noreply.github.com>
2024-10-02 08:16:25 +0530

    error boundry and notfound pages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,16 +6,22 @@ import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+    throw new Error('Error is bounnded')
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
 
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
 });
 console.log('FormSchema :>> ', FormSchema);
 // Use Zod to update the expected types
@@ -23,58 +29,68 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 console.log('UpdateInvoice :>> ', UpdateInvoice);
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
- 
-  const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+`;
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-  console.log('object :>> ', CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  }));
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    console.log('object :>> ', CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }));
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
     // const rawFormData = {
     //     customerId: formData.get('customerId'),
     //     amount: formData.get('amount'),
     //     status: formData.get('status'),
     //   };
-      // const rawFormDatass = Object.fromEntries(formData.entries())
-      // Test it out:
-      // console.log(rawFormData);
-      // console.log('rawFormDatass :>> ', rawFormDatass);
-      // console.log(typeof rawFormData.amount,'type of amt')
-      console.log('amountInCents :>> ', amountInCents);
-      await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-    
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    // const rawFormDatass = Object.fromEntries(formData.entries())
+    // Test it out:
+    // console.log(rawFormData);
+    // console.log('rawFormDatass :>> ', rawFormDatass);
+    // console.log(typeof rawFormData.amount,'type of amt')
+
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
     }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
 
